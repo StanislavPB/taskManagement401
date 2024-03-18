b54c0cde157b1651bf95bf38a962eb4a1a2994c0
StanislavPB <perminovsb@gmail.com>
2024-03-18 21:29:42 +0000

    Initial commit

---
+++ src/main/java/org/taskmanagement401/TaskManagement401Application.java
@@ -0,0 +1,13 @@
+package org.taskmanagement401;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagement401Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagement401Application.class, args);
+	}
+
+}
