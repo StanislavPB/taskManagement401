3e7414db52f1abb90ccf2f393855c71f1b43eb29
maria-romaniuk <romaniuk.mary@gmail.com>
2024-03-19 13:51:53 +0100

    create_branch+entityTask

---
+++ src/main/java/entity/Task.java
@@ -0,0 +1,13 @@
+package entity;
+
+import java.time.LocalDate;
+
+public class Task {
+    private String id;
+    private String name;
+    private LocalDate endDate;
+//    private List<User> assignedUsers;
+//    private Priority priority;
+//    private boolean completed;
+//    private List<Comment> comments;
+}
