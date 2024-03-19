34b86bfe328276870dac3e6aa3123e5c01a6cff5
zhekazp <zhekazp21@gmail.com>
2024-03-19 22:19:31 +0200

    Menu

---
+++ src/main/java/org/taskmanagement401/service/AuthorizationService.java
@@ -0,0 +1,36 @@
+package org.taskmanagement401.service;
+
+import org.taskmanagement401.config.ErrorCodes;
+import org.taskmanagement401.dto.ResponseDTO;
+import org.taskmanagement401.dto.UserDto;
+import org.taskmanagement401.dto.error.ErrorDto;
+import org.taskmanagement401.entity.User;
+import org.taskmanagement401.repository.UserRepository;
+import org.taskmanagement401.service.util.UserTalkService;
+import org.taskmanagement401.service.validation.UserValidation;
+
+import java.util.List;
+import java.util.Optional;
+
+public class AuthorizationService {
+    private UserValidation validation=new UserValidation();
+    private UserRepository repository;
+
+    public AuthorizationService(UserRepository repository) {
+        this.repository = repository;
+    }
+    public ResponseDTO verification(){
+        UserDto dto= UserTalkService.getUserParameters(false);
+        Optional<User>user =repository.getUserIdByLoginAndPassword(dto);
+        List<ErrorDto> errors=validation.checkUser(dto);
+        if(user.isEmpty()){
+            errors.add(new ErrorDto(ErrorCodes.WRONGLOGINDATA.getStatusCode(),
+                    ErrorCodes.WRONGLOGINDATA.getDescription()));
+            return new ResponseDTO<>(400,errors);
+        }else{
+            return new ResponseDTO<>(200,user.get());
+        }
+
+    }
+
+}
