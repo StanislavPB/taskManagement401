77e84d4e1e5ac88911a3a83dc6c9c72bdd4e1c12
Vitalii-Chaplygin <chapliginvitaliy@gmail.com>
2024-03-21 13:07:37 +0100

    commit Vitalii Comment12

--- src/main/java/org/taskmanagement401/service/menuClasses/employeeMenu/CommentMenu.java
+++ src/main/java/org/taskmanagement401/service/menuClasses/employeeMenu/CommentMenu.java
@@ -3,6 +3,7 @@ package org.taskmanagement401.service.menuClasses.employeeMenu;
 import org.taskmanagement401.dto.CommentDto;
 import org.taskmanagement401.dto.ResponseDTO;
 import org.taskmanagement401.entity.Project;
+import org.taskmanagement401.entity.Task;
 import org.taskmanagement401.entity.User;
 import org.taskmanagement401.repository.CommentRepository;
 import org.taskmanagement401.repository.ProjectRepository;
@@ -10,6 +11,7 @@ import org.taskmanagement401.repository.TaskRepository;
 import org.taskmanagement401.repository.UserRepository;
 import org.taskmanagement401.service.CommentService;
 import org.taskmanagement401.service.util.PrintActiveProjects;
+import org.taskmanagement401.service.util.PrintUserTasks;
 import org.taskmanagement401.service.util.UserInput;
 import org.taskmanagement401.service.util.UserTalkService;
 
@@ -26,14 +28,14 @@ public class CommentMenu {
         UserTalkService.printAllProjects(projectList);
        int i = UserInput.inputPositiveInt(" Choose project by id");*/
 
-     Optional<Project> projectOptional= PrintActiveProjects.print(projectRepository);
-        if (projectOptional.isEmpty()){
+     Optional<Task> taskOptional= PrintUserTasks.print(user);
+        if (taskOptional.isEmpty()){
             System.out.println("Wrong input data");
 
         }else {
               CommentDto commentDto = new CommentDto(UserInput.inputText("Enter new comment"));
              CommentService commentService =new CommentService(commentRepository);
-            ResponseDTO responseDTO =commentService.addNewComment(user,commentDto,projectOptional.get());
+            ResponseDTO responseDTO =commentService.addNewComment(user,commentDto,taskOptional.get());
           }
         }
 
