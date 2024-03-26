bf2f5873fb758bc30ab4cad80e7e7c4ae620e101
zhekazp <zhekazp21@gmail.com>
2024-03-26 10:41:04 +0200

    Menu

--- src/main/java/org/taskmanagement401/service/AddUserToProjectService.java
+++ src/main/java/org/taskmanagement401/service/AddUserToProjectService.java
@@ -1,16 +1,30 @@
 package org.taskmanagement401.service;
 
+import org.taskmanagement401.config.ErrorCodes;
 import org.taskmanagement401.dto.ResponseDTO;
+import org.taskmanagement401.dto.error.ErrorDto;
 import org.taskmanagement401.entity.Project;
 import org.taskmanagement401.entity.Task;
 import org.taskmanagement401.entity.User;
 import org.taskmanagement401.service.dataService.save.SaveProject;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class AddUserToProjectService {
     public static ResponseDTO assignUser (Project project, User user){
+        List<ErrorDto> errors=new ArrayList<>();
         project.getUsers().add(user);
         SaveProject saveProject=new SaveProject();
-        saveProject.saveAddUser(project,user);
-        return new ResponseDTO<>(200, "Status ok");
+        Optional<Exception> result=saveProject.saveAddUser(project,user);
+        if(result.isPresent()){
+            errors.add(new ErrorDto(ErrorCodes.DATASAVING.getStatusCode(),
+                    ErrorCodes.DATASAVING.getDescription()+result.get().getMessage()));
+            return new ResponseDTO<>(400,errors);
+        }else {
+            return new ResponseDTO<>(200, "Status ok");
+        }
+
     }
 }
